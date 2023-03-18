6357a3f64ce6b57c7c195fa9a4b594d7813756d4
Tehillah <tehillahk@outlook.com>
2023-03-17 22:09:08 -0500

    new  file

--- src/components/InputHero.tsx
+++ src/components/InputHero.tsx
@@ -4,11 +4,9 @@ const InputHero = (props: any) => {
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+          <textarea className="textarea textarea-bordered py-6" placeholder="Enter query">
+            
+          </textarea>
           <button className="btn-primary btn">Get Started</button>
         </div>
       </div>
